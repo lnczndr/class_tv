272de821daa6b4dceb32bc2de9792eeb9838fa3f
unknown <lanacazandra@gmail.com>
2023-05-18 00:05:18 +0800

    pseudocode

---
+++ tv.py
@@ -0,0 +1,18 @@
+# LANA CAZANDRA U. LEGASPI - BSCPE 1-5
+# CLASS TV
+
+# creating class "tv"
+
+    # defining methods for:
+# turning on the tv
+# turning off the tv
+# getting channel
+# setting the channel
+# getting the volume
+# setting volume
+# making the channel go up
+# making the channel go down
+# increasing the volume (up)
+# decreasing the volume (down)
+
+# output
