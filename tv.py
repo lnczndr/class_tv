aacfff4c24d6e30b8c68b29c6b5c38c611d785a5
unknown <lanacazandra@gmail.com>
2023-05-18 16:33:49 +0800

    defining and initializing variables

--- tv.py
+++ tv.py
@@ -2,17 +2,22 @@
 # CLASS TV
 
 # creating class "tv"
-
+class TV:
     # defining methods for:
-# turning on the tv
-# turning off the tv
-# getting channel
-# setting the channel
-# getting the volume
-# setting volume
-# making the channel go up
-# making the channel go down
-# increasing the volume (up)
-# decreasing the volume (down)
+    def __init__ (self, power, channel, volume):
+        self.power = False
+        self.channel = 0
+        self.volume = 0
+        
+    # turning on the tv
+    # turning off the tv
+    # getting channel
+    # setting the channel
+    # getting the volume
+    # setting volume
+    # making the channel go up
+    # making the channel go down
+    # increasing the volume (up)
+    # decreasing the volume (down)
 
 # output
