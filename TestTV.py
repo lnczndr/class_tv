a0306fce03568b6af0fa64a52ab11567181e977f
unknown <lanacazandra@gmail.com>
2023-05-18 20:19:22 +0800

    pseudocode for TestTV

---
+++ TestTV.py
@@ -0,0 +1,11 @@
+# LANA CAZANDRA U. LEGASPI - BSCPE 1-5
+
+
+# Import the tv
+# Make 2 TV object
+# Turn on the TV
+# Set a channel
+# Get the current channel
+# Set the volume
+# Get the current volume
+# Turn off the TV
