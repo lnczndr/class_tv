30fec7148f2ac7ed19abe714a669bb1ae965fcbb
unknown <lanacazandra@gmail.com>
2023-05-18 20:34:34 +0800

    setting TV 1 object and showing its output

--- TestTV.py
+++ TestTV.py
@@ -4,9 +4,26 @@
 from tv import TV
 
 # Make 2 TV object
+# Making the first TV object
+tv1 = TV()
+
 # Turn on the TV
+tv1.power_on()
+
 # Set a channel
+tv1.set_channel (30)
+
 # Get the current channel
+current_channel_tv1 = tv1.get_channel
+
 # Set the volume
+tv1.set_volume(3)
+
 # Get the current volume
+current_channel_tv1 = tv1.get_volume()
+
 # Turn off the TV
+tv1.power_off()
+
+# Make the second TV object
+# Repeat the process
